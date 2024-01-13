73b1f12450c0fb177f01db5d5f2d411c8260e300
Elizandro <elizandro.nascimento@di2win.com>
2024-01-13 12:39:20 -0300

    Teste de extracao

---
+++ Extractor.py
@@ -0,0 +1,21 @@
+import tabula
+import json
+
+def pdf_to_json(pdf_path, json_path):
+    # Extrai tabelas do PDF
+    tables = tabula.read_pdf(pdf_path, pages='all', multiple_tables=True)
+
+    # Converte as tabelas para JSON
+    json_data = []
+    for table_num, table in enumerate(tables):
+        table_json = table.to_dict(orient='records')
+        json_data.append({f"table_{table_num + 1}": table_json})
+
+    # Escreve o JSON para um arquivo
+    with open(json_path, 'w', encoding='utf-8') as json_file:
+        json.dump(json_data, json_file, ensure_ascii=False, indent=2)
+
+if __name__ == "__main__":
+    pdf_path = "C:/Users/MICRO/Documents/TESTES EM PYTHON - EXTRAÇÃO/In_path/PDF.pdf"
+    json_path = "C:/Users/MICRO/Documents/TESTES EM PYTHON - EXTRAÇÃO/Out_path/saida1.json"
+    pdf_to_json(pdf_path, json_path)
